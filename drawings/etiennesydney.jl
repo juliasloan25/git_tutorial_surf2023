04926c583d66688809119102be10f14b0d4649ea
sydneyvernon <sydneyv@gmail.com>
2023-06-30 14:16:16 -0700

    added sloth

---
+++ drawings/etiennesydney.jl
@@ -0,0 +1,8 @@
+# Use the emojis in the emoji bank to make a drawing. Fill in lines 5-7.
+# Leave the line start comments, but you can remove the outline if you want.
+
+#  ---------------------------
+# |                          
+# |                          
+# | 🦥                      
+#  ---------------------------
